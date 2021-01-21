2c8a3409fc78667b3cafbc67f2d9c63730e16068
MaryWenzel <wenzel.mary1993@gmail.com>
2021-01-21 14:29:53 -0600

    this branch initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import axios from 'axios';
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+    <SearchPage />
       </header>
     </div>
   );
