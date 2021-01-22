8ecd207155d3a74ac884f8238a77d4215174f60e
nblack0917 <nickablack@gmail.com>
2021-01-22 12:21:48 -0600

    commit

--- src/JustSearchBar.js
+++ src/JustSearchBar.js
@@ -8,107 +8,7 @@ class JustSearchBar extends Component {
         super(props)
 
         this.state = {
-            arrayOfStories: [
-                {
-                id: 1,
-                created_at: "2006-10-09T18:21:51.000Z",
-                author: "pg",
-                title: "Y Combinator",
-                url: "http://ycombinator.com",
-                text: null,
-                points: 57,
-                parent_id: null,
-                children: 
-                [
-                {
-                id: 15,
-                created_at: "2006-10-09T19:51:01.000Z",
-                author: "sama",
-                text: "&#34;the rising star of venture capital&#34; -unknown VC eating lunch on SHR",
-                points: 5,
-                parent_id: 1,
-                children: 
-                [
-                {
-                id: 17,
-                created_at: "2006-10-09T19:52:45.000Z",
-                author: "pg",
-                text: "Is there anywhere to eat on Sandhill Road?",
-                points: 5,
-                parent_id: 15,
-                children: [ ]
-                }
-                ]
-                }
-                ]
-                },
-                {
-                    id: 2,
-                    created_at: "2006-10-09T18:21:51.000Z",
-                    author: "pg",
-                    title: "Y Combinator",
-                    url: "http://ycombinator.com",
-                    text: null,
-                    points: 57,
-                    parent_id: null,
-                    children: 
-                    [
-                    {
-                    id: 15,
-                    created_at: "2006-10-09T19:51:01.000Z",
-                    author: "sama",
-                    text: "&#34;the rising star of venture capital&#34; -unknown VC eating lunch on SHR",
-                    points: 5,
-                    parent_id: 1,
-                    children: 
-                    [
-                    {
-                    id: 17,
-                    created_at: "2006-10-09T19:52:45.000Z",
-                    author: "pg",
-                    text: "Is there anywhere to eat on Sandhill Road?",
-                    points: 5,
-                    parent_id: 15,
-                    children: [ ]
-                    }
-                    ]
-                    }
-                    ]
-                    },
-                    {
-                        id: 3,
-                        created_at: "2006-10-09T18:21:51.000Z",
-                        author: "pg",
-                        title: "Y Combinator",
-                        url: "http://ycombinator.com",
-                        text: null,
-                        points: 57,
-                        parent_id: null,
-                        children: 
-                        [
-                        {
-                        id: 15,
-                        created_at: "2006-10-09T19:51:01.000Z",
-                        author: "sama",
-                        text: "&#34;the rising star of venture capital&#34; -unknown VC eating lunch on SHR",
-                        points: 5,
-                        parent_id: 1,
-                        children: 
-                        [
-                        {
-                        id: 17,
-                        created_at: "2006-10-09T19:52:45.000Z",
-                        author: "pg",
-                        text: "Is there anywhere to eat on Sandhill Road?",
-                        points: 5,
-                        parent_id: 15,
-                        children: [ ]
-                        }
-                        ]
-                        }
-                        ]
-                        },
-            ],
+            arrayOfStories: [],
             question: "",
             searchState: "",
             dropdown: "term",
@@ -144,7 +44,7 @@ class JustSearchBar extends Component {
         axios.get(searchTerm)
             .then( res => {
                 const arrayOfStories = res.data.hits
-                // this.setState({ arrayOfStories })
+                this.setState({ arrayOfStories })
             })
     }
 
